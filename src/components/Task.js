00c8093e9683c6397d33fbd02be3f0b804f530a1
Samuel Kirori <samuelkirori300@gmail.com>
2024-04-28 12:00:13 +0300

    Done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-function Task() {
+function Task({ task, handleDelete }) {
+  const handleDeleteTask = () => {
+    handleDelete(task.text);
+  };
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="text">{task.text}</div>
+      <div className="label">{task.category}</div>
+      <button className="delete" onClick={handleDeleteTask}>
+        X
+      </button>
     </div>
   );
 }
